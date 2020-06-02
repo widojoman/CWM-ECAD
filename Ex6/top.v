d74b62006f31d2f231d004dd0bfea3b0055bf53f
noaz <noaz@users.noreply.github.com>
2020-06-02 02:05:12 +0100

    Added exercises 6 and 7

---
+++ Ex6/top.v
@@ -0,0 +1,22 @@
+//////////////////////////////////////////////////////////////////////////////////
+// Exercise #6 
+// Student Name:
+// Date: 
+//
+//
+//  Description: In this exercise, you need to design a multiplexer between a dice and traffic 
+//  lights, where the output acts according to the following truth table:
+//
+//  sel | out
+// -----------------
+//   0  | dice
+//   1  | traffic lights
+//
+//  inputs:
+//           rst, clk, button,sel
+//
+//  outputs:
+//           result[2:0]
+//
+//////////////////////////////////////////////////////////////////////////////////
+
