335575c2dbda5435fcc3c1ecb0e565fde345b0fa
Cloud User <centos@ip-172-31-5-205.eu-west-2.compute.internal>
2020-06-08 11:00:10 +0000

    finished ex1

--- Ex1/top.v
+++ Ex1/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #1 
-// Student Name:
-// Date: 
+// Student Name: Matthew Widojo
+// Date: 6/8/2020
 //
 //  Description: In this exercise, you need to design a logical unit, where the  
 //  output acts according to the following truth table:
@@ -32,5 +32,8 @@ module logicalunit(
     wire   out;
          
       //Todo: add you logic here
+	
+	assign out= ~ a ? (b ? func[3] : func[2]) : (b ? func[1] : func[0]);
+	
 
 endmodule
