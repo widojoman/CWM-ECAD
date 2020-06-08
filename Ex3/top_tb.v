d27cc89d2fde0b4af72f5ae77a0f57b3d81de347
widojoman <matthewwidojo.MAW@gmail.com>
2020-06-08 16:00:17 +0000

    task 3 draft

--- Ex3/top_tb.v
+++ Ex3/top_tb.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Test bench for Exercise #3 - Up/Down counter
-// Student Name:
-// Date: 
+// Student Name:Matthew
+// Date: 6/8/2020
 //
 // Description: A testbench module to test Ex3 - counter
 // Guidance: start with simple tests of the module (how should it react to each 
@@ -14,15 +14,53 @@ module top_tb(
     );
     
 //Todo: Parameters
+    parameter CLK_PERIOD = 10; //sets clock period, of 10 time units
+    parameter Test_period = 100; //in terms of time period, how long will the counter run for
+				//(goes up and down back to zero)
 
-//Todo: Regitsers and wires
-
+//Todo: Registers and wires
+    reg clk;
+    reg rst;
+    reg direction;
+    reg enable;
+    reg err;
+    wire [2:0] counter_out;
 //Todo: Clock generation
+    initial
+    begin
+       clk = 1'b0;
+       forever
+         #(CLK_PERIOD/2) clk=~clk;
+    end
 
 //Todo: User logic
-    
+    initial
+    begin
+    	rst=1; //reset counter to start
+    	direction = 1;
+	enable = 1;
+	err = 0;
+
+	#1 rst=0; //rst to zero so it works
+
+    end
+
 //Todo: Finish test, check for success
+    initial
+    begin
+    	#Test_period 
+	
+
+    	$finish;
+    end
 
 //Todo: Instantiate counter module
- 
-endmodule 
+    counter top(
+	.rst (rst),
+	.enable (enable),
+	.direction (direction),
+	.clk (clk),
+	.counter_out (counter_out[2:0])
+	);
+
+endmodule
