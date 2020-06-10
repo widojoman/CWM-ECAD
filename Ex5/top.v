16f745dcd130983775bafce20487c8136d50f2d7
widojoman <matthewwidojo.MAW@gmail.com>
2020-06-10 08:51:59 +0000

    finished ex5

--- Ex5/top.v
+++ Ex5/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #5 
-// Student Name:
-// Date: 
+// Student Name:Matt
+// Date: 6/10/2020
 //
 //  Description: In this exercise, you need to implement a UK traffic lights 
 //  sequencing system. 
@@ -12,4 +12,49 @@
 //  outputs:
 //           red, amber, green
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
 
+module lights(
+    //Todo: add ports 
+    input clk,
+    output reg red=1'b1,
+    output reg amber=1'b0,
+    output reg green=1'b0
+    );               
+    //Todo: add registers and wires, if needed
+
+    //Todo: add user logic
+
+    
+    always@(posedge clk)
+    if ((red==1'b0 && amber==1'b0 && green==1'b0)
+	||(red==1'b1 && amber==1'b0 && green==1'b1)
+	||(red==1'b0 && amber==1'b1 && green==1'b1)
+	||(red==1'b1 && amber==1'b1 && green==1'b1)) 
+    begin
+        red <=1'b1;
+	amber <=1'b0;
+	green <=1'b0;
+    end else if(red==1'b1 && amber==1'b0 && green==1'b0)begin
+        red <=1'b1;
+	amber <=1'b1;
+	green <=1'b0;
+    end else if(red==1'b1 && amber==1'b1 && green==1'b0)begin
+        red <=1'b0;
+	amber <=1'b0;
+	green <=1'b1;
+    end else if(red==1'b0 && amber==1'b0 && green==1'b1)begin
+        red <=1'b0;
+	amber <=1'b1;
+	green <=1'b0;
+    end else begin
+        red <=1'b1;
+	amber <=1'b0;
+	green <=1'b0;
+    end
+    
+		  
+    
+ 
+    
+endmodule
--- Ex5/top_tb.v
+++ Ex5/top_tb.v
@@ -1,9 +1,81 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Test bench for Exercise #5 - Traffic Lights
-// Student Name:
-// Date: 
+// Student Name:Matt
+// Date: 6/10/2020
 //
 // Description: A testbench module to test Ex5 - Traffic Lights
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
 
+module top_tb(
+    );
+    
+//Todo: Parameters
+    parameter CLK_PERIOD = 10; //sets clock period, of 10 time units
+    parameter Test_period = 100; //in terms of time period, how long will the counter run for
+
+//Todo: Registers and wires
+    reg clk;
+    reg err;
+    reg [2:0] prev_counter;
+    wire red;
+    wire amber;
+    wire green;
+
+  
+    
+//Todo: Clock generation
+    initial
+    begin
+       clk = 1'b0;
+       forever
+         #(CLK_PERIOD/2) clk=~clk;
+    end
+
+//Todo: User logic
+    initial begin
+	err = 0;
+
+	//Tests
+	forever begin
+		prev_counter = {red,amber,green};	   
+		#CLK_PERIOD;
+		if ((red==1'b0 && amber==1'b0 && green==1'b0)
+			||(red==1'b1 && amber==1'b0 && green==1'b1)
+			||(red==1'b0 && amber==1'b1 && green==1'b1)
+			||(red==1'b1 && amber==1'b1 && green==1'b1)) 
+			begin
+   	   		$display("Test Returns invalid numbers");
+   	   		err=1;
+       		end
+
+		if (prev_counter == {red,amber,green})
+			begin
+   	   		$display("Test didn't change");
+   	   		err=1;
+       		end
+		
+	end
+	end
+
+//Todo: Finish test, check for success
+    initial
+    begin
+    	#Test_period 
+    	if (err==0)
+    	   $display("***Test Passed :)");
+	
+
+    	$finish;
+    end
+
+//Todo: Instantiate counter module
+    lights top(
+	.clk (clk),
+	.red (red),
+	.amber (amber),
+	.green (green)
+	);
+
+endmodule
